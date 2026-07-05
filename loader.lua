affbcc589cfefe613254a0b6362acadae540fc3c
albertobombardi01-beep <albertobombardi01@gmail.com>
2026-07-05 14:27:48 +0200

    'Loader'

--- loader.lua
+++ loader.lua
@@ -1,5 +1,6 @@
 print(game.PlaceId)
-local gameslist = loadstring(game:HttpGet("https://raw.githubusercontent.com/albertobombardi01-beep/Loader/refs/heads/main/gameslist.lua"))()
+local randomtoken = math.random(111111,999999)
+local gameslist = loadstring(game:HttpGet("https://raw.githubusercontent.com/albertobombardi01-beep/Loader/master/gameslist.lua?t=".. randomtoken))()
 
 local isInGame = false
 for _, gameListed: number in gameslist do
