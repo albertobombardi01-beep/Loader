ec531fbde7e89cb94e1f8216f200d2e49a1b9c58
albertobombardi01-beep <albertobombardi01@gmail.com>
2026-07-05 14:45:26 +0200

    'Loader'

--- games/139992937215031.lua
+++ games/139992937215031.lua
@@ -1,2 +1,8 @@
 local randomtoken = math.random(111111,999999)
 local uilib = loadstring(game:HttpGet("https://raw.githubusercontent.com/albertobombardi01-beep/Loader/master/ui.lua?t=".. randomtoken))()
+
+local window = uilib:CreateWindow("Drop Balls For Brainrots")
+local section = window:addSection("Main")
+section:addButton("Click me!", function()
+    print("Button clicked!")
+end)
