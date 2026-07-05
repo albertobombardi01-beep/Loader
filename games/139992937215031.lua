20e3f7565efbf5ed95de33442214db35e09dc70a
albertobombardi01-beep <albertobombardi01@gmail.com>
2026-07-05 14:42:12 +0200

    'Loader'

---
+++ games/139992937215031.lua
@@ -0,0 +1,2 @@
+local randomtoken = math.random(111111,999999)
+local uilib = loadstring(game:HttpGet("https://raw.githubusercontent.com/albertobombardi01-beep/Loader/master/ui.lua?t=".. randomtoken))()
